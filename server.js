75b285a8667a4e270bb2d96b5d9d566a2807641a
Glitch (spark-freedom) <none>
2019-09-18 18:24:09 +0000

    🙈💋 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+
+// require and use "multer"...
+var multer = require('multer');
+// var upload = multer( {dest: 'uploads/'} );
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+
+var upload = multer({ storage: storage })
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res, next){ 
+  
+  if (!req.file) {
+    res.send("Please upload a file");
+  }
+  
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype, 
+    size: req.file.size
+  })
+})
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
